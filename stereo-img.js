fdb92402c7065ab5baad29b90ffbf5572ed738eb
Steren <steren.giannini@gmail.com>
2021-09-30 04:46:53 +0000

    my first home made custom element

--- stereo-img.js
+++ stereo-img.js
@@ -1,8 +1,41 @@
 import { parse } from './modules/vr180-parser/vr180-parser.js';
 
-window.onload = init;
+let tmpl = document.createElement('template');
+tmpl.innerHTML = `
+  <img src="demo.vr.jpg"></img>
+`;
 
-async function init() {
-    let parsedResults = await parse('demo.vr.jpg');
-    console.log(parsedResults);
-}
+class StereoImg extends HTMLElement {
+
+    get src() {
+      return this.getAttribute('src');
+    }
+    set src(val) {
+      // Reflect the value of the open property as an HTML attribute.
+      if (val) {
+        this.setAttribute('src', val);
+      } else {
+        this.removeAttribute('open');
+      }
+    }
+  
+    constructor() {
+      super();
+
+      this.addEventListener('click', e => {
+        console.log('I was clicked')
+      });
+
+      let shadowRoot = this.attachShadow({mode: 'open'});
+      shadowRoot.appendChild(tmpl.content.cloneNode(true));
+    }
+  }
+
+window.customElements.define('stereo-img', StereoImg);
+
+// window.onload = init;
+
+// async function init() {
+//     let parsedResults = await parse('demo.vr.jpg');
+//     console.log(parsedResults);
+// }
