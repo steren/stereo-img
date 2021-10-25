8ba536f7761c577198ad2af3eb9f02b66ab9dfc8
Steren <steren.giannini@gmail.com>
2021-10-25 06:22:20 +0000

    stereo: read focal length from exif and use better defaults

--- modules/stereo-parser/stereo-parser.js
+++ modules/stereo-parser/stereo-parser.js
@@ -1,12 +1,13 @@
+import exifr from 'exifr'
+
 /**
  * fetch the image from URL
  * read its left half, return in left eye image
  * read its right half, return in right eye image  
  * */
 async function parseStereo(url) {
-  const response = await fetch(url);
-  const blob = await response.blob();
-  const image = await createImageBitmap(blob);
+  const image = await createImageFromURL(url);
+  
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
   const width = image.width;
@@ -14,20 +15,53 @@ async function parseStereo(url) {
   canvas.width = width;
   canvas.height = height;
   ctx.drawImage(image, 0, 0);
+
+  const exif = await exifr.parse(url, {
+    xmp: true,
+    multiSegment: true
+  })
   
   const leftEye = ctx.getImageData(0, 0, width / 2, height);
   const rightEye = ctx.getImageData(width / 2, 0, width / 2, height);
   
-  // Assume a 180deg horizontal and 90 deg vertical field of view, 
-  const phiLength = Math.PI;
-  const thetaStart = Math.PI / 4;
-  const thetaLength =  Math.PI / 2;
+  let angleOfViewFocalLengthIn35mmFormat = function(focalLengthIn35mmFormat) {
+    // https://en.wikipedia.org/wiki/Angle_of_view#Common_lens_angles_of_view
+    // https://en.wikipedia.org/wiki/35_mm_equivalent_focal_length
+    // angle of view on the diagonal (35mm is 24 mm (vertically) × 36 mm (horizontal), giving a diagonal of about 43.3 mm)
+    const diagonalAngle = 2 * Math.atan(43.3 / (2 * focalLengthIn35mmFormat));
+    // Pi / 4 for a square.
+    const halfAngle = Math.atan(height / (width / 2));
+    const horizontalAngle = diagonalAngle * Math.cos(halfAngle);
+    const verticalAngle = diagonalAngle * Math.sin(halfAngle);
+
+    return {diagonalAngle, horizontalAngle, verticalAngle};
+  }
+
+  let phiLength;
+  let thetaLength;
+
+
+  if(exif?.FocalLengthIn35mmFormat) {
+    const angle = angleOfViewFocalLengthIn35mmFormat(exif.FocalLengthIn35mmFormat);
+    phiLength = angle.horizontalAngle;
+    thetaLength = angle.verticalAngle;
+  } else if(exif?.Make === 'GoPro' || url.includes('gopro') || url.includes('GOPR') ) {
+    // GoPro (https://gopro.com/help/articles/question_answer/hero7-field-of-view-fov-information?sf96748270=1)
+    phiLength = 2.1397737;
+    thetaLength = 1.647591;
+  } else {
+    const assumeFocalLengthIn35mmFormat = 27;
+    const angle = angleOfViewFocalLengthIn35mmFormat(assumeFocalLengthIn35mmFormat);
+    phiLength = angle.horizontalAngle;
+    thetaLength = angle.verticalAngle;
+  }
+
+  const thetaStart = Math.PI / 2 - thetaLength / 2;
 
   return {leftEye, rightEye, phiLength, thetaStart, thetaLength};
 }
 
-async function createImageBitmap(blob) {
-  const url = URL.createObjectURL(blob);
+async function createImageFromURL(url) {
   const image = new Image();
   image.src = url;
   return new Promise((resolve, reject) => {
